9c9ff988395e36b6e0e1180f4dc606ef9a900c7e
Straple <hadayegor@gmail.com>
2023-08-10 17:30:22 +0300

    some fix

--- tests/unit/test_utils.hpp
+++ tests/unit/test_utils.hpp
@@ -8,4 +8,4 @@
 #include <unordered_map>
 
 template <typename K = int, typename V = int, typename H = std::hash<K>>
-using map_t = hashmap<K, V, H>;
+using map_t = /*заменяем здесь*/ hashmap /*и тестируем*/<K, V, H>;
